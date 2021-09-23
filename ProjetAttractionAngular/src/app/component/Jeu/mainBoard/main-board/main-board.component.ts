2b4d6edb30248d1de5ecf18477acf28ff801b6a7
Julien <julien22130@gmail.com>
2021-09-23 14:24:24 +0200

    Auto stash before merge of "main" and "origin/main"

--- ProjetAttractionAngular/src/app/component/Jeu/mainBoard/main-board/main-board.component.ts
+++ ProjetAttractionAngular/src/app/component/Jeu/mainBoard/main-board/main-board.component.ts
@@ -6,36 +6,34 @@ import { GestionAchatService } from 'src/app/service/GestionJeu/gestion-achat.se
 @Component({
   selector: 'app-main-board',
   templateUrl: './main-board.component.html',
-  styleUrls: ['./main-board.component.css']
+  styleUrls: ['./main-board.component.css'],
 })
 export class MainBoardComponent implements OnInit {
-
-  typeMeteo:string="";
-  meteo:number=0;
-  argentGagne:number=0;
-  argentDepense:number=0;
-  nbIncident:number=0;
-  nbVisiteur:number=0;
-  prixEntree:number=50;
+  meteo: string = '';
+  argentGagne: number = 0;
+  argentDepense: number = 0;
+  nbIncident: number = 0;
+  nbVisiteur: number = 0;
+  prixEntree: number = 50;
 
   constructor(
     private gestionAchatService: GestionAchatService,
-    private gestionParcService: GestionParcService) {
-   }
+    private gestionParcService: GestionParcService
+  ) {}
 
   ngOnInit(): void {}
 
-  public getParc():Parc{
-    const storage = localStorage.getItem("parcChosen");
-    if(storage){
+  public getParc(): Parc {
+    const storage = localStorage.getItem('parcChosen');
+    if (storage) {
       return JSON.parse(storage);
     }
-    return new Parc("probleme","probleme");
+    return new Parc('probleme', 'probleme');
   }
 
-  public debutPartie(): boolean{
-    const storage = localStorage.getItem("parcChosen");
-    if(storage){
+  public debutPartie(): boolean {
+    const storage = localStorage.getItem('parcChosen');
+    if (storage) {
       let parc = JSON.parse(storage);
       if(parc.nbjour>0){
         return false;
@@ -45,78 +43,112 @@ export class MainBoardComponent implements OnInit {
 }
 
   public finJournee() {
-    const storage = localStorage.getItem("parcChosen");
-    let prixFonctionnement=0;
-    let revenuJourPersonne=0;
-    let attractivite=0;
-    let salaire=0;
-    let capaciteMax=0;
+    const storage = localStorage.getItem('parcChosen');
+    let prixFonctionnement = 0;
+    let revenuJourPersonne = 0;
+    let attractivite = 0;
+    let salaire = 0;
+    let capaciteMax = 0;
     let impactEmployeCapacite = 2;
     let impactEmployeAttractivite = 2;
 
-    if (storage){
+    if (storage) {
       let parc = JSON.parse(storage);
-      this.gestionAchatService.getByTypeElementAndParc("employe",parc).subscribe(
-        (achatEmployes) => {
-          achatEmployes.forEach(
-            achatEmploye => {
-              if (achatEmploye.typeElement==="employe" && achatEmploye.element.salaire && achatEmploye.element.salaire>50){
+      this.gestionAchatService
+        .getByTypeElementAndParc('employe', parc)
+        .subscribe(
+          (achatEmployes) => {
+            achatEmployes.forEach((achatEmploye) => {
+              if (
+                achatEmploye.typeElement === 'employe' &&
+                achatEmploye.element.salaire &&
+                achatEmploye.element.salaire > 50
+              ) {
                 impactEmployeCapacite += achatEmploye.element.salaire;
                 salaire += achatEmploye.element.salaire;
-			        }
-              else if (achatEmploye.typeElement==="employe" && achatEmploye.element.salaire && achatEmploye.element.salaire>50){
+              } else if (
+                achatEmploye.typeElement === 'employe' &&
+                achatEmploye.element.salaire &&
+                achatEmploye.element.salaire > 50
+              ) {
                 impactEmployeAttractivite += achatEmploye.element.salaire;
                 salaire += achatEmploye.element.salaire;
               }
-            }
-          );
+            });
 
-          this.gestionAchatService.getByParc(parc).subscribe(
-            (achats) => {
-              achats.forEach(
-                achat => {
-                  if (achat.typeElement!=="employe" && achat.typeElement!=="commodite"){
-                    let aleatoire = Math.round(Math.random()*10);
+            this.gestionAchatService.getByParc(parc).subscribe(
+              (achats) => {
+                achats.forEach((achat) => {
+                  if (
+                    achat.typeElement !== 'employe' &&
+                    achat.typeElement !== 'commodite'
+                  ) {
+                    let aleatoire = Math.round(Math.random() * 10);
                     let incidentElement = 1;
-                    if (aleatoire<(achat.element.tauxIncident!/10)){
-                      this.nbIncident ++;
-                      incidentElement -= 1/impactEmployeCapacite;
+                    if (aleatoire < achat.element.tauxIncident! / 10) {
+                      this.nbIncident++;
+                      incidentElement -= 1 / impactEmployeCapacite;
                     }
-                    if (achat.niveauAmelioration && achat.niveauAmelioration!==0 && achat.element.nbAmelioration){
-                      attractivite += achat.niveauAmelioration / achat.element.nbAmelioration;
+                    if (
+                      achat.niveauAmelioration &&
+                      achat.niveauAmelioration !== 0 &&
+                      achat.element.nbAmelioration
+                    ) {
+                      attractivite +=
+                        achat.niveauAmelioration / achat.element.nbAmelioration;
                     }
-                    capaciteMax += incidentElement*achat.element.affluence!;
+                    capaciteMax += incidentElement * achat.element.affluence!;
                     prixFonctionnement += achat.element.prixFonctionnement!;
-                    if (achat.typeElement==="restaurant" || achat.typeElement==="boutique"){
+                    if (
+                      achat.typeElement === 'restaurant' ||
+                      achat.typeElement === 'boutique'
+                    ) {
                       revenuJourPersonne += achat.element.revenuJourPersonne!;
                     }
                   }
+                });
+                attractivite += achats.length / 100;
+                if (impactEmployeAttractivite !== 0) {
+                  attractivite += 1 / impactEmployeAttractivite;
                 }
-              );
-              attractivite += achats.length/100;
-              if (impactEmployeAttractivite!==0){attractivite += 1/impactEmployeAttractivite;}
-              if (attractivite>1){attractivite=1;}
-              else if (attractivite<0){attractivite=0.1;}
+                if (attractivite > 1) {
+                  attractivite = 1;
+                } else if (attractivite < 0) {
+                  attractivite = 0.1;
+                }
 
-              switch (Math.round(Math.random()*4)){
-                case 0 : this.typeMeteo = "beaucoup plu";
-                  this.nbVisiteur = Math.round(capaciteMax*attractivite*0.5);break;
-                case 1 : this.typeMeteo = "un peu plu";
-                  this.nbVisiteur = Math.round(capaciteMax*attractivite*0.7);break;
-                case 2 : this.typeMeteo = "fait nuageux";
-                  this.nbVisiteur = Math.round(capaciteMax*attractivite*0.9);break;
-                case 3 : this.typeMeteo = "fait beau";
-                  this.nbVisiteur =  Math.round(capaciteMax*attractivite);break;
-                case 4 : this.typeMeteo = "fait très chaud";
-                  this.nbVisiteur = Math.round(capaciteMax*attractivite*0.7);break;
-                default : console.log("pb dans calcul météo (fin de journee)")
-              }
+                switch (Math.round(Math.random() * 4)) {
+                  case 0:
+                    this.meteo = 'Forte pluie ⛈️';
+                    this.nbVisiteur = capaciteMax * attractivite * 0.5;
+                    break;
+                  case 1:
+                    this.meteo = 'Faible pluie 🌧️';
+                    this.nbVisiteur = capaciteMax * attractivite * 0.7;
+                    break;
+                  case 2:
+                    this.meteo = 'Nuageux ☁️';
+                    this.nbVisiteur = capaciteMax * attractivite * 0.9;
+                    break;
+                  case 3:
+                    this.meteo = 'Belles éclaircies ⛅';
+                    this.nbVisiteur = capaciteMax * attractivite;
+                    break;
+                  case 4:
+                    this.meteo = 'Ensoleillé ☀️';
+                    this.nbVisiteur = capaciteMax * attractivite * 0.7;
+                    break;
+                  default:
+                    console.log('pb dans calcul météo (fin de journee)');
+                }
 
-              this.argentGagne = this.nbVisiteur*(this.prixEntree + revenuJourPersonne);
-              this.argentDepense = salaire + prixFonctionnement;
+                this.argentGagne =
+                  this.nbVisiteur * (this.prixEntree + revenuJourPersonne);
+                this.argentDepense = salaire + prixFonctionnement;
 
-              parc.argent = parc.argent + this.argentGagne - this.argentDepense;
-              parc.nbjour ++;
+                parc.argent =
+                  parc.argent + this.argentGagne - this.argentDepense;
+                parc.nbjour++;
 
               this.gestionParcService.save(parc).subscribe(
                 (parcSave) => localStorage.setItem("parcChosen",JSON.stringify(parcSave)),
@@ -130,6 +162,4 @@ export class MainBoardComponent implements OnInit {
       );
     }
   }
-
-
 }
--- ProjetAttractionAngular/src/app/component/Jeu/mainBoard/map/map.component.ts
+++ ProjetAttractionAngular/src/app/component/Jeu/mainBoard/map/map.component.ts
@@ -4,48 +4,57 @@ import { Parc } from 'src/app/model/parc';
 import { Element } from 'src/app/model/element';
 import { GestionAchatService } from 'src/app/service/GestionJeu/gestion-achat.service';
 
-
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
+  parcStorage = localStorage.getItem('parcChosen');
 
-   parcStorage = localStorage.getItem('parcChosen');
+  elements: Element[] = [];
+  mapSize: number = 0;
 
-
-   elements: Element[]=[];
-   mapSize: number = 0;
-
-  constructor(
-    private gestionAchatService: GestionAchatService
-  ) {
-     if (this.parcStorage) {
-       this.gestionAchatService
-         .getByParc(JSON.parse(this.parcStorage))
-         .subscribe(
-           (res) => {
-             res.forEach((achat) => {
-               if (achat.typeElement !== 'employe') {
+  constructor(private gestionAchatService: GestionAchatService) {
+    if (this.parcStorage) {
+      this.gestionAchatService
+        .getByParc(JSON.parse(this.parcStorage))
+        .subscribe(
+          (res) => {
+            res.forEach((achat) => {
+              if (achat.typeElement !== 'employe') {
                 //  achat.element.typeElement=achat.typeElement;
-                 this.elements.push(achat.element);
-               }
-             });
-           },
-           (error) => console.log(error)
-         );
-     }
-     this.mapSize=this.getParc().taille!;
+                if (achat.typeElement == 'commodite') {
+                  console.log(achat);
+                  for (let i = 0; i < achat.nbSameElement; i++) {
+                    this.elements.push(achat.element);
+                  }
+                }
+
+                this.elements.push(achat.element);
+              }
+            });
+          },
+          (error) => console.log(error)
+        );
+    }
+    this.mapSize = this.getParc().taille!;
   }
 
   ngOnInit(): void {}
 
-   public getParc(): Parc {
+  public getParc(): Parc {
     let parc = localStorage.getItem('parcChosen');
-     if (parc) {
-       return JSON.parse(parc);
-     }
-     return new Parc('probleme', 'probleme');
-   }
+    if (parc) {
+      return JSON.parse(parc);
+    }
+    return new Parc('probleme', 'probleme');
+  }
+
+  public empty(): boolean {
+    if (this.elements.length == 0) {
+      return true;
+    }
+    return false;
+  }
 }
